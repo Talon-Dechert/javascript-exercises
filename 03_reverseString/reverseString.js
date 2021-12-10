434f014d8f5abb8e77770f0784d01625643f30a1
Talon Dechert <techdechert@outlook.com>
2021-12-09 21:11:43 -0800

    Learning TDD pt 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
 const reverseString = function(str) {
+    if (str == null){
+        return ('ERROR');
+    } else {
 
+        let newString = '';
+        for (i = (str.length - 1); i >= 0; i--){
+            newString = newString + str[i];
+        }
+        return (newString);
+    };
 };
 
 // Do not edit below this line
