48b826066eba5fd49d60f52f35ec7782f0cbd744
Talon Dechert <techdechert@outlook.com>
2021-12-12 10:48:47 -0800

    Learning testing pt4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numToRemove) {
 
+    if (numToRemove.length > 1){
+        for (i = 0; i < numToRemove.length; i++){
+            let innerNumToRemove = numToRemove[i];
+            for (x = 0; x < arr.length; x++){
+                if (innerNumToRemove === arr[x]){
+                    arr.splice(x, 1);
+                }
+            }
+        }
+    } else {
+        for (x = 0; x < arr.length; x++){
+            if (numToRemove[0] === arr[x]){
+                arr.splice(x, 1);
+            }
+            
+        }
+    }
+    return(arr);
 };
 
 // Do not edit below this line
