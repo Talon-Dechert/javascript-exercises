791bec9f924282d71fe0582a620b27068c35e43c
Talon Dechert <techdechert@outlook.com>
2021-12-14 21:32:15 -0800

    Learning tests pt 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let totalSum = 0;
 
+    if (typeof(firstNum) !== "number" || typeof(secondNum) !== "number"){
+        console.log(typeof(firstNum));
+        console.log(typeof(secondNum));
+        return ("ERROR");
+    } else if (firstNum < 0 || secondNum < 0) {
+        return("ERROR");
+    } else if (firstNum === secondNum){
+        totalSum = firstNum + secondNum;
+        return (totalSum);
+    } else {
+        for (i = firstNum; i <= secondNum; i++){
+            totalSum += i;
+        };
+        for (i = secondNum; i <= firstNum; i++){
+            totalSum += i;
+        }
+        return (totalSum);
+    }
 };
 
 // Do not edit below this line
