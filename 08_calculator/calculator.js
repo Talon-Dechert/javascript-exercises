afd31899719873f29eafb0cdb5d867098ece7a38
Talon Dechert <techdechert@outlook.com>
2022-05-28 10:29:42 -0700

    Simple calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return(x +y);
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return(x-y);
 };
 
-const sum = function() {
-	
+const sum = function([...x]) {
+	if (x.length == 0) return 0;
+  y = 0;
+  for (i=0; i < x.length; i++){
+    y += x[i];
+  }
+  return y;
 };
 
-const multiply = function() {
-
+const multiply = function([...x]) {
+  if (x.length == 0) return 0;
+  y = 1;
+  for (i = 0; i < x.length; i++){
+    y *= x[i];
+  }
+  return y;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let pow = 1;
+	for (i = 0; i < y; i++){
+    pow *= x;
+  }
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if(x == 0) return 1;
+  if(x == 1) return 1;
+
+  let fact = 1;
+	for (i = x; i > 0; i--){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
