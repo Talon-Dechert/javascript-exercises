29c52af5aed80df75f82622c0d15b24a377ab4ec
Talon Dechert <techdechert@outlook.com>
2022-05-28 13:32:32 -0700

    Set up initial function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (words) {
+
+    //* Setting up comparable arrays
+    words = words.toLowerCase();
+    let wordsArray = words.split('');
+    let reverseArray = [];
+
+    //! Need to remove punctuation and spaces
+
+    wordsArray.forEach(element => reverseArray.push(element));
+
+    reverseArray.reverse();
+
+    //! Now to compare arrays - most likely through a forEach comparison
+
 
 };
+
+palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
