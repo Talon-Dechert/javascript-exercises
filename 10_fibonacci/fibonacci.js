154d1645c29b23488ff9c5bcd53adbdb1f000399
Talon Dechert <techdechert@outlook.com>
2022-05-28 14:19:11 -0700

    Setup fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) return("OOPS");
+    if (num == 0) return 0;
 
+    let prevprev = 0;
+    let prev = 1;
+    let curr = 1;
+
+    for (i = 1; i < num; i++){
+        curr = prevprev + prev;
+        prevprev = prev;
+        prev = curr;
+    }
+
+    console.log(curr);
+    return curr;
 };
+
+fibonacci(8);
 
 // Do not edit below this line
 module.exports = fibonacci;
